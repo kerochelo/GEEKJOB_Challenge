5aad581cdc7675bbcd35ae2ac6d1ad21efa4647a
takahiro kanno <kerochelo@gmail.com>
2017-07-12 14:10:22 +0900

    DB操作/特定のレコードの情報取得と表示の課題を提出します。

---
+++ CampChallenge_java/018.DB操作/db4Copy.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.db.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author takahirokanno
+ */
+public class db4 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.lang.ClassNotFoundException
+     * @throws java.lang.InstantiationException
+     * @throws java.lang.IllegalAccessException
+     * @throws java.sql.SQLException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            Connection db_con = null;
+            PreparedStatement db_st = null;
+            ResultSet db_data;
+            
+            try{
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                db_con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db", "kerochelo", "tkhr8547");
+                
+                db_st = db_con.prepareStatement("select * from profiles where profileID = ?");
+                db_st.setInt(1, 1);
+                
+                db_data= db_st.executeQuery();
+                
+                while(db_data.next()){
+                    out.print("profileID:" + db_data.getInt("profileID") + "<br>");
+                    out.print("名前:" + db_data.getString("name") + "<br>");
+                    out.print("電話番号:" + db_data.getString("tell") + "<br>");
+                    out.print("年齢:" + db_data.getInt("age") + "<br>");
+                    out.print("生年月日:" + db_data.getString("birthday") + "<br>");
+                }
+                db_data.close();
+                db_st.close();
+                db_con.close();
+            }
+            catch(SQLException  |  ClassNotFoundException  | InstantiationException |  IllegalAccessException e_sql){
+                out.print("エラーが発生しました。" + e_sql.toString());
+            }
+            finally{
+                if(db_con != null){
+                    try{
+                        db_con.close();
+                    }
+                    catch(SQLException e){
+                        System.out.print(e.getMessage());
+                    }
+                }
+            }
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet db4</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            Logger.getLogger(db4.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex) {
+            Logger.getLogger(db4.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
